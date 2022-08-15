8ca935b4e9363338bde9cb0e0fb832692111604e
VJ1996 <virajpatel.1996@gmail.com>
2022-08-15 00:37:07 -0400

    Wired up Data to the API allowing for getting of basic User data information. Added user controller and UserData and UserModel and hooked up SQL data access for the models

---
+++ RMDataManager/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using TRMDataManager.Library.DataAccess;
+using TRMDataManager.Library.Models;
+
+namespace RMDataManager.Controllers
+{
+    [Authorize]
+    public class UserController : ApiController
+    {
+        public List<UserModel> GetById()
+        {
+            string userId = RequestContext.Principal.Identity.GetUserId(); 
+            //not passing in string Id. Do it this way to prevent people from guessing
+            //for peoples user ids and stealing info
+
+            UserData data = new UserData();
+
+            return data.GetUserById(userId);
+        }
+
+        
+    }
+}
